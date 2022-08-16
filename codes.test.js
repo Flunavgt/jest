d707b031b827b7970c3b33619c479fe97de1e891
Federico <fluna1973@yahoo.com>
2022-08-16 15:55:27 -0600

    results completed

--- codes.test.js
+++ codes.test.js
@@ -29,11 +29,39 @@ describe('Task 3', () => {
     expect(operate.add(a, b)).toBe(10);
   });
 
+  it('add', () => {
+    const a = 10,
+      b = 5;
+
+    expect(operate.add(a, b)).toBe(15);
+  });
+
+  it('add', () => {
+    const a = 75,
+      b = 25;
+
+    expect(operate.add(a, b)).toBe(100);
+  });
+
   it('rest', () => {
     const a = 8,
       b = 6;
 
     expect(operate.subtract(a, b)).toBe(2);
+  });
+
+  it('rest', () => {
+    const a = 810,
+      b = 10;
+
+    expect(operate.subtract(a, b)).toBe(800);
+  });
+
+  it('rest', () => {
+    const a = 200,
+      b = 60;
+
+    expect(operate.subtract(a, b)).toBe(140);
   });
 
   it('multiply', () => {
@@ -43,18 +71,46 @@ describe('Task 3', () => {
     expect(operate.multiply(a, b)).toBe(4);
   });
 
+  it('multiply', () => {
+    const a = 12,
+      b = 12;
+
+    expect(operate.multiply(a, b)).toBe(144);
+  });
+
+  it('multiply', () => {
+    const a = 20,
+      b = 6;
+
+    expect(operate.multiply(a, b)).toBe(120);
+  });
+
   it('divide', () => {
     const a = 15,
       b = 5;
 
     expect(operate.divide(a, b)).toBe(3);
   });
+
+  it('divide', () => {
+    const a = 150,
+      b = 5;
+
+    expect(operate.divide(a, b)).toBe(30);
+  });
+
+  it('divide', () => {
+    const a = 90,
+      b = 3;
+
+    expect(operate.divide(a, b)).toBe(30);
+  });
 });
 
 describe('task 4',() => {
 it('captal letter', () => {
-  const string = 'capital'
+  const string = 'federico'
 
-  expect(firstCap(string)).toBe("Capital");
+  expect(firstCap(string)).toBe("Federico");
 });
 });
